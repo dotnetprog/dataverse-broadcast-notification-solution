5009df5e945dc3c4e54229c6a5233508629e0681
dotnetprog <24593889+dotnetprog@users.noreply.github.com>
2025-09-04 08:12:44 -0400

    fix pr reporter

--- src/broadcast-typescript/vitest.config.ts
+++ src/broadcast-typescript/vitest.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { VitestMarkdownReporter } from "vitest-markdown-reporter";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     include: ['tests/**/*.test.ts', 'tests/**/*.spec.ts'],
     environment: 'jsdom',
-    reporters: ["default","junit", new VitestMarkdownReporter()],
+    reporters: ["default","junit"],
     outputFile: {
        markdown: "test-coverage.md", // Specify the output file name
        junit:'vitest-junit.xml'
